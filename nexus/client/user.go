5ec54958a3ab6383c09a8998ff8a65b20a500b23
Thomas Krahn <ntbc@gmx.net>
2020-01-15 17:44:23 +0100

    Optimize Client user code

--- nexus/client/user.go
+++ nexus/client/user.go
@@ -30,8 +30,7 @@ func userIOReader(user User) (io.Reader, error) {
 		return nil, fmt.Errorf("could not marshal user data: %v", err)
 	}
 
-	result := bytes.NewReader(b)
-	return result, nil
+	return bytes.NewReader(b), nil
 }
 
 func (c client) UserCreate(user User) error {
@@ -42,67 +41,84 @@ func (c client) UserCreate(user User) error {
 
 	body, resp, err := c.Post(usersAPIEndpoint, ioReader)
 	if err != nil {
-		return fmt.Errorf("could not create user '%s': %v", user.UserID, err)
+		return err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("could not create user '%s': HTTP: %d, %s", user.UserID, resp.StatusCode, string(body))
+		return fmt.Errorf("%s", string(body))
 	}
 
 	return nil
 }
 
-func (c client) UserRead(userId string) (*User, error) {
+func (c client) UserRead(id string) (*User, error) {
 	body, resp, err := c.Get(usersAPIEndpoint, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("could not get users: HTTP: %d, %s", resp.StatusCode, string(body))
+		return nil, fmt.Errorf("%s", string(body))
 	}
 
 	var users []User
 	if err := json.Unmarshal(body, users); err != nil {
-		return nil, fmt.Errorf("could not unmarschal user '%s': %v", userId, err)
+		return nil, fmt.Errorf("could not unmarschal users: %v", err)
 	}
 
 	for _, user := range users {
-		if user.UserID == userId {
+		if user.UserID == id {
 			return &user, nil
 		}
 	}
 
-	return nil, nil
+	return nil, fmt.Errorf("could not find user '%s'", id)
 }
 
-func (c client) UserUpdate(userId string, user User) error {
+func (c client) UserUpdate(id string, user User) error {
+	// Not sure what this is and why is required to update a user
 	if user.Source == "" {
 		user.Source = "default"
 	}
+
 	ioReader, err := userIOReader(user)
 	if err != nil {
 		return err
 	}
-	body, resp, err := c.Put(fmt.Sprintf("%s/%s", usersAPIEndpoint, userId), ioReader)
+
+	body, resp, err := c.Put(fmt.Sprintf("%s/%s", usersAPIEndpoint, id), ioReader)
 	if err != nil {
 		return err
 	}
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-		return fmt.Errorf("could not update user '%s': HTTP: %d, %s", userId, resp.StatusCode, string(body))
+		return fmt.Errorf("%s", string(body))
 	}
 
 	return nil
 }
 
-func (c client) UserDelete(userId string) error {
-	body, resp, err := c.Delete(fmt.Sprintf("%s/%s", usersAPIEndpoint, userId))
+func (c client) UserDelete(id string) error {
+	body, resp, err := c.Delete(fmt.Sprintf("%s/%s", usersAPIEndpoint, id))
 	if err != nil {
 		return err
 	}
+
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
-		return fmt.Errorf("could not delete user '%s': HTTP: %d, %s", userId, resp.StatusCode, string(body))
+		return fmt.Errorf("%s", string(body))
 	}
 	return err
+}
+
+// UserChangePassword  must be send with content-type text/plain :-/
+func (c client) UserChangePassword(id string, password string) error {
+	data := bytes.NewReader([]byte(password))
+	body, resp, err := c.Put(fmt.Sprintf("%s/%s/change-password", usersAPIEndpoint, id), data)
+	if err != nil {
+		return fmt.Errorf("could not change password of user '%s': %v", id, err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("could not change password of user '%s':  HTTP: %d, %s ", id, resp.StatusCode, string(body))
+	}
+	return nil
 }
