4ff1ed4a4f670f1318d70cc44a021a26f8b4ad6a
Thomas Krahn <ntbc@gmx.net>
2020-01-22 07:41:38 +0100

    Add PreCheck for Terraform acceptance tests

--- nexus/provider_test.go
+++ nexus/provider_test.go
@@ -1,6 +1,7 @@
 package nexus
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -27,4 +28,16 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_impl(t *testing.T) {
 	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("NEXUS_URL"); v == "" {
+		t.Fatalf("NEXUS_URL must be set for acceptance tests")
+	}
+	if v := os.Getenv("NEXUS_USERNAME"); v == "" {
+		t.Fatalf("NEXUS_USERNAME must be set for acceptance tests")
+	}
+	if v := os.Getenv("NEXUS_PASSWORD"); v == "" {
+		t.Fatalf("NEXUS_PASSWORD must be set for acceptance tests")
+	}
 }
