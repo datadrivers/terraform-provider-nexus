b1008e94e3e6748ae05addc2644501c601355386
Thomas Krahn <ntbc@gmx.net>
2020-03-24 21:31:25 +0100

    Use Nexus 3.21.2 for testing

--- scripts/start-nexus.sh
+++ scripts/start-nexus.sh
@@ -1,20 +1,30 @@
-#!/bin/bash -e
+#!/bin/bash
+set -eo pipefail
 
 echo "Starting Nexus container..."
-docker run -d --rm --name nexus -p 127.0.0.1:8081:8081 sonatype/nexus3:3.20.1
+docker run -d --rm --name nexus -p 127.0.0.1:8081:8081 sonatype/nexus3:3.21.2
 
-echo -n "Waiting for Nexus to be ready "
-i=1
-until wget -t 1 http://127.0.0.1:8081 -O /dev/null -q
-do
-    sleep 1
-    echo -n .
-    if [[ $((i%3)) == 0 ]]; then echo -n ' '; fi
-    (( i++ ))
-done
+function wait_for_nexus {
+    echo -n "Waiting for Nexus to be ready "
+    i=1
+    until wget -t 1 http://127.0.0.1:8081 -O /dev/null -q
+    do
+        sleep 1
+        echo -n .
+        if [[ $((i%3)) == 0 ]]; then echo -n ' '; fi
+        (( i++ ))
+    done
+}
+
+wait_for_nexus
 
 echo "Getting admin password..."
 NEXUS_ADMIN_PASSWORD=$(docker exec -ti nexus cat /nexus-data/admin.password)
 
 echo "Setting admin password..."
 curl -X PUT "http://127.0.0.1:8081/service/rest/beta/security/users/admin/change-password" -H "accept: application/json" -H "Content-Type: text/plain" -d "admin123" -u "admin:${NEXUS_ADMIN_PASSWORD}"
+
+echo "Enabling scripting related features and restarting Nexus. https://issues.sonatype.org/browse/NEXUS-23205"
+docker exec nexus /bin/bash -c "echo 'nexus.scripts.allowCreation=true' >> /nexus-data/etc/nexus.properties" && docker restart nexus
+
+wait_for_nexus
