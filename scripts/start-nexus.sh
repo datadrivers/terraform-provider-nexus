d7a1a9f5516517c57bcbe2662cc3fd3457c52805
Ivan Remizov <ivan.remizov@mendix.com>
2020-04-24 18:03:53 +0200

    Refactor nexus init sequence
    - Use nexus.properties file instead of file modification.
    - Don't restart nexus because configuration is done with properties
    file and not after nexus is started.
    - Don't start container with random password, use default password123
    - Disable Startup Wizard.

--- scripts/start-nexus.sh
+++ scripts/start-nexus.sh
@@ -4,7 +4,14 @@ set -eo pipefail
 export NEXUS_VERSION=3.22.1
 
 echo "Starting Nexus v${NEXUS_VERSION} container..."
-docker run -d --rm --name nexus -l terraform-provider-nexus=true -p 127.0.0.1:8081:8081 "sonatype/nexus3:${NEXUS_VERSION}"
+docker run \
+    -d \
+    --rm \
+    --name nexus \
+    -l terraform-provider-nexus=true \
+    -p 127.0.0.1:8081:8081 \
+    -v "${PWD}/nexus.properties:/nexus-data/etc/nexus.properties" \
+    "sonatype/nexus3:${NEXUS_VERSION}"
 
 function wait_for_nexus {
     echo -n "Waiting for Nexus to be ready "
@@ -13,23 +20,12 @@ function wait_for_nexus {
     do
         sleep 1
         echo -n .
-        if [[ $((i%3)) == 0 ]]; then echo -n ' '; fi
+        if [[ $((i%5)) == 0 ]]; then echo -n ' '; fi
         (( i++ ))
-        echo ""
     done
+    echo ""
 }
 
 wait_for_nexus
 
-echo "Getting admin password..."
-NEXUS_ADMIN_PASSWORD=$(docker exec -ti nexus cat /nexus-data/admin.password)
-
-echo "Setting admin password..."
-curl -X PUT "http://127.0.0.1:8081/service/rest/beta/security/users/admin/change-password" -H "accept: application/json" -H "Content-Type: text/plain" -d "admin123" -u "admin:${NEXUS_ADMIN_PASSWORD}"
-
-echo "Enabling scripting related features and restarting Nexus. https://issues.sonatype.org/browse/NEXUS-23205"
-docker exec nexus /bin/bash -c "echo 'nexus.scripts.allowCreation=true' >> /nexus-data/etc/nexus.properties" && docker restart nexus
-
-wait_for_nexus
-
-echo "Nexus is started"
+echo "Nexus is started."
