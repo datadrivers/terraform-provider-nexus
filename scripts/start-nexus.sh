85c4bbbbbdde9f3ee4e8726dd20e4da4d29e8c18
Rüdiger Block <ruediger.block@gmx.de>
2020-05-27 12:29:41 +0200

    revert 8a2e356443061719eccc6fb38f2a268b42fef2b0 for scripts/start-nexus.sh

--- scripts/start-nexus.sh
+++ scripts/start-nexus.sh
@@ -29,10 +29,4 @@ function wait_for_nexus {
 
 wait_for_nexus
 
-echo "Getting admin password..."
-NEXUS_ADMIN_PASSWORD=$(docker exec -ti nexus cat /nexus-data/admin.password)
-
-echo "Setting admin password..."
-curl -X PUT "http://127.0.0.1:${LOCAL_PORT}/service/rest/beta/security/users/admin/change-password" -H "accept: application/json" -H "Content-Type: text/plain" -d "admin123" -u "admin:${NEXUS_ADMIN_PASSWORD}"
-
 echo "Nexus is started."
