23c38279762d1278b3203044ca0d606cc3b116f6
Thomas Krahn <ntbc@gmx.net>
2020-06-05 13:41:38 +0200

    Revert sort parameter of method resourceDataStringSlice

--- nexus/tools.go
+++ nexus/tools.go
@@ -3,19 +3,15 @@ package nexus
 import (
 	"fmt"
 	"os"
-	"sort"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func resourceDataStringSlice(d *schema.ResourceData, attribute string, sorted bool) []string {
+func resourceDataStringSlice(d *schema.ResourceData, attribute string) []string {
 	n := d.Get(fmt.Sprintf("%s.#", attribute)).(int)
 	data := make([]string, n)
 	for i := 0; i < n; i++ {
 		data[i] = d.Get(fmt.Sprintf("%s.%d", attribute, i)).(string)
-	}
-	if sorted {
-		sort.Strings(data)
 	}
 	return data
 }
