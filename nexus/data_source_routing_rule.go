ad6588d2c64cdfabaf38ed250ffbe9e363fd0456
Andre Moeller <andre.moeller@datadrivers.de>
2021-01-20 12:17:11 +0100

    feat: Add resource and data source nexus_routing_rules Fixes #91

---
+++ nexus/data_source_routing_rule.go
@@ -0,0 +1,51 @@
+/*
+Use this data source to work with routing rules
+
+Example Usage
+
+```hcl
+data "nexus_routing_rule" "stop_leaks" {
+  name = "stop-leaks"
+}
+```
+*/
+package nexus
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceRoutingRule() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRoutingRuleRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: "The name of the routing rule",
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+			"description": {
+				Computed:    true,
+				Description: "The description of the routing rule",
+				Type:        schema.TypeString,
+			},
+			"mode": {
+				Computed:    true,
+				Description: "The mode describe how to hande with mathing requests. Possible values: `BLOCK` or `ALLOW`",
+				Type:        schema.TypeString,
+			},
+			"matchers": {
+				Computed:    true,
+				Description: "Matchers is a list of regular expressions used to identify request paths that are allowed or blocked (depending on above mode)",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Type:        schema.TypeSet,
+			},
+		},
+	}
+}
+
+func dataSourceRoutingRuleRead(d *schema.ResourceData, m interface{}) error {
+	d.SetId(d.Get("name").(string))
+	return resourceRoutingRuleRead(d, m)
+}
---
+++ nexus/data_source_routing_rule_test.go
@@ -0,0 +1,46 @@
+package nexus
+
+import (
+	"fmt"
+	"testing"
+
+	nexus "github.com/datadrivers/go-nexus-client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourceRoutingRule(t *testing.T) {
+	resName := "data.nexus_routing_rule.acceptance"
+	rule := nexus.RoutingRule{
+		Name:        "acceptance",
+		Description: "acceptance test",
+		Mode:        "BLOCK",
+		Matchers: []string{
+			"^/com/example/.*",
+			"^/org/example/.*",
+		},
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceRoutingRuleConfig(rule) + testAccDataSourceRoutingRuleConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resName, "name", rule.Name),
+					resource.TestCheckResourceAttr(resName, "description", rule.Description),
+					resource.TestCheckResourceAttr(resName, "mode", string(rule.Mode)),
+					resource.TestCheckResourceAttrSet(resName, "matchers.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceRoutingRuleConfig() string {
+	return fmt.Sprintf(`
+data "nexus_routing_rule" "acceptance" {
+	name = nexus_routing_rule.acceptance.name
+}
+`)
+}
