1c845d3ea26c9d687cdba98956e0192eed416028
Andre Moeller <andre.moeller@datadrivers.de>
2021-08-13 15:23:45 +0200

    Add support for SAML SSO (Nexus Pro only)

---
+++ docs/resources/security_saml.md
@@ -0,0 +1,51 @@
+---
+layout: "nexus"
+page_title: "Nexus: nexus_security_saml"
+sidebar_current: "docs-nexus-resource-security_saml"
+description: |-
+  ---
+**NOTE**
+---
+
+# nexus_security_saml
+
+---
+**NOTE**
+
+PRO Feature
+
+---
+
+Use this resource to create a Nexus Security SAML
+
+## Example Usage
+
+```hcl
+resource "nexus_security_saml" "example" {
+  idp_metadata                  = "<EntityDescriptor ...>...</EntityDescriptor>"
+  entity_id                     = "http://nexus.example/service/rest/v1/security/saml/metadata"
+  validate_response_signature   = true
+  validate_assertion_signature  = true
+  username_attribute            = "username"
+  first_name_attribute          = "firstName"
+  last_name_attribute           = "lastName
+  email_attribute               = "email
+  groups_attribute              = "groups"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `idp_metadata` - (Required) SAML Identity Provider Metadata XML
+* `email_attribute` - (Optional) IdP field mappings for user's email address
+* `entity_id` - (Optional) Entity ID URI
+* `first_name_attribute` - (Optional) IdP field mappings for user's given name
+* `groups_attribute` - (Optional) IdP field mappings for user's groups
+* `last_name_attribute` - (Optional) IdP field mappings for user's family name
+* `username_attribute` - (Optional) IdP field mappings for username
+* `validate_assertion_signature` - (Optional) By default, if a signing key is found in the IdP metadata, then NXRM will attempt to validate signatures on the assertions.
+* `validate_response_signature` - (Optional) By default, if a signing key is found in the IdP metadata, then NXRM will attempt to validate signatures on the response.
+
+
---
+++ nexus/data_source_security_saml_test.go
@@ -0,0 +1,58 @@
+package nexus
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	nexus "github.com/datadrivers/go-nexus-client"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func testAccDataSourceSecuritySaml() nexus.SAML {
+	return nexus.SAML{
+		// https://samltest.id/saml/idp
+		IdpMetadata:                "<EntityDescriptor xmlns=\"urn:oasis:names:tc:SAML:2.0:metadata\" ID=\"SAMLtestIdP\" xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\" xmlns:shibmd=\"urn:mace:shibboleth:metadata:1.0\" xmlns:xml=\"http://www.w3.org/XML/1998/namespace\" xmlns:mdui=\"urn:oasis:names:tc:SAML:metadata:ui\" validUntil=\"2100-01-01T00:00:42Z\" entityID=\"https://samltest.id/saml/idp\">\n    <IDPSSODescriptor protocolSupportEnumeration=\"urn:oasis:names:tc:SAML:2.0:protocol urn:oasis:names:tc:SAML:1.1:protocol urn:mace:shibboleth:1.0\">\n        <Extensions>\n            <shibmd:Scope regexp=\"false\">samltest.id</shibmd:Scope>\n            <mdui:UIInfo>\n                <mdui:DisplayName xml:lang=\"en\">SAMLtest IdP</mdui:DisplayName>\n                <mdui:Description xml:lang=\"en\">A free and basic IdP for testing SAML deployments</mdui:Description>\n                <mdui:Logo height=\"90\" width=\"225\">https://samltest.id/saml/logo.png</mdui:Logo>\n            </mdui:UIInfo>\n        </Extensions>\n\n        <KeyDescriptor use=\"signing\">\n            <ds:KeyInfo>\n                    <ds:X509Data>\n                        <ds:X509Certificate>\nMIIDETCCAfmgAwIBAgIUZRpDhkNKl5eWtJqk0Bu1BgTTargwDQYJKoZIhvcNAQEL\nBQAwFjEUMBIGA1UEAwwLc2FtbHRlc3QuaWQwHhcNMTgwODI0MjExNDEwWhcNMzgw\nODI0MjExNDEwWjAWMRQwEgYDVQQDDAtzYW1sdGVzdC5pZDCCASIwDQYJKoZIhvcN\nAQEBBQADggEPADCCAQoCggEBAJrh9/PcDsiv3UeL8Iv9rf4WfLPxuOm9W6aCntEA\n8l6c1LQ1Zyrz+Xa/40ZgP29ENf3oKKbPCzDcc6zooHMji2fBmgXp6Li3fQUzu7yd\n+nIC2teejijVtrNLjn1WUTwmqjLtuzrKC/ePoZyIRjpoUxyEMJopAd4dJmAcCq/K\nk2eYX9GYRlqvIjLFoGNgy2R4dWwAKwljyh6pdnPUgyO/WjRDrqUBRFrLQJorR2kD\nc4seZUbmpZZfp4MjmWMDgyGM1ZnR0XvNLtYeWAyt0KkSvFoOMjZUeVK/4xR74F8e\n8ToPqLmZEg9ZUx+4z2KjVK00LpdRkH9Uxhh03RQ0FabHW6UCAwEAAaNXMFUwHQYD\nVR0OBBYEFJDbe6uSmYQScxpVJhmt7PsCG4IeMDQGA1UdEQQtMCuCC3NhbWx0ZXN0\nLmlkhhxodHRwczovL3NhbWx0ZXN0LmlkL3NhbWwvaWRwMA0GCSqGSIb3DQEBCwUA\nA4IBAQBNcF3zkw/g51q26uxgyuy4gQwnSr01Mhvix3Dj/Gak4tc4XwvxUdLQq+jC\ncxr2Pie96klWhY/v/JiHDU2FJo9/VWxmc/YOk83whvNd7mWaNMUsX3xGv6AlZtCO\nL3JhCpHjiN+kBcMgS5jrtGgV1Lz3/1zpGxykdvS0B4sPnFOcaCwHe2B9SOCWbDAN\nJXpTjz1DmJO4ImyWPJpN1xsYKtm67Pefxmn0ax0uE2uuzq25h0xbTkqIQgJzyoE/\nDPkBFK1vDkMfAW11dQ0BXatEnW7Gtkc0lh2/PIbHWj4AzxYMyBf5Gy6HSVOftwjC\nvoQR2qr2xJBixsg+MIORKtmKHLfU\n                        </ds:X509Certificate>\n                    </ds:X509Data>\n            </ds:KeyInfo>\n\n        </KeyDescriptor>\n        <KeyDescriptor use=\"signing\">\n            <ds:KeyInfo>\n                    <ds:X509Data>\n                        <ds:X509Certificate>\nMIIDEjCCAfqgAwIBAgIVAMECQ1tjghafm5OxWDh9hwZfxthWMA0GCSqGSIb3DQEB\nCwUAMBYxFDASBgNVBAMMC3NhbWx0ZXN0LmlkMB4XDTE4MDgyNDIxMTQwOVoXDTM4\nMDgyNDIxMTQwOVowFjEUMBIGA1UEAwwLc2FtbHRlc3QuaWQwggEiMA0GCSqGSIb3\nDQEBAQUAA4IBDwAwggEKAoIBAQC0Z4QX1NFKs71ufbQwoQoW7qkNAJRIANGA4iM0\nThYghul3pC+FwrGv37aTxWXfA1UG9njKbbDreiDAZKngCgyjxj0uJ4lArgkr4AOE\njj5zXA81uGHARfUBctvQcsZpBIxDOvUUImAl+3NqLgMGF2fktxMG7kX3GEVNc1kl\nbN3dfYsaw5dUrw25DheL9np7G/+28GwHPvLb4aptOiONbCaVvh9UMHEA9F7c0zfF\n/cL5fOpdVa54wTI0u12CsFKt78h6lEGG5jUs/qX9clZncJM7EFkN3imPPy+0HC8n\nspXiH/MZW8o2cqWRkrw3MzBZW3Ojk5nQj40V6NUbjb7kfejzAgMBAAGjVzBVMB0G\nA1UdDgQWBBQT6Y9J3Tw/hOGc8PNV7JEE4k2ZNTA0BgNVHREELTArggtzYW1sdGVz\ndC5pZIYcaHR0cHM6Ly9zYW1sdGVzdC5pZC9zYW1sL2lkcDANBgkqhkiG9w0BAQsF\nAAOCAQEASk3guKfTkVhEaIVvxEPNR2w3vWt3fwmwJCccW98XXLWgNbu3YaMb2RSn\n7Th4p3h+mfyk2don6au7Uyzc1Jd39RNv80TG5iQoxfCgphy1FYmmdaSfO8wvDtHT\nTNiLArAxOYtzfYbzb5QrNNH/gQEN8RJaEf/g/1GTw9x/103dSMK0RXtl+fRs2nbl\nD1JJKSQ3AdhxK/weP3aUPtLxVVJ9wMOQOfcy02l+hHMb6uAjsPOpOVKqi3M8XmcU\nZOpx4swtgGdeoSpeRyrtMvRwdcciNBp9UZome44qZAYH1iqrpmmjsfI9pJItsgWu\n3kXPjhSfj1AJGR1l9JGvJrHki1iHTA==\n                        </ds:X509Certificate>\n                    </ds:X509Data>\n            </ds:KeyInfo>\n\n        </KeyDescriptor>\n        <KeyDescriptor use=\"encryption\">\n            <ds:KeyInfo>\n                    <ds:X509Data>\n                        <ds:X509Certificate>\nMIIDEjCCAfqgAwIBAgIVAPVbodo8Su7/BaHXUHykx0Pi5CFaMA0GCSqGSIb3DQEB\nCwUAMBYxFDASBgNVBAMMC3NhbWx0ZXN0LmlkMB4XDTE4MDgyNDIxMTQwOVoXDTM4\nMDgyNDIxMTQwOVowFjEUMBIGA1UEAwwLc2FtbHRlc3QuaWQwggEiMA0GCSqGSIb3\nDQEBAQUAA4IBDwAwggEKAoIBAQCQb+1a7uDdTTBBFfwOUun3IQ9nEuKM98SmJDWa\nMwM877elswKUTIBVh5gB2RIXAPZt7J/KGqypmgw9UNXFnoslpeZbA9fcAqqu28Z4\nsSb2YSajV1ZgEYPUKvXwQEmLWN6aDhkn8HnEZNrmeXihTFdyr7wjsLj0JpQ+VUlc\n4/J+hNuU7rGYZ1rKY8AA34qDVd4DiJ+DXW2PESfOu8lJSOteEaNtbmnvH8KlwkDs\n1NvPTsI0W/m4SK0UdXo6LLaV8saIpJfnkVC/FwpBolBrRC/Em64UlBsRZm2T89ca\nuzDee2yPUvbBd5kLErw+sC7i4xXa2rGmsQLYcBPhsRwnmBmlAgMBAAGjVzBVMB0G\nA1UdDgQWBBRZ3exEu6rCwRe5C7f5QrPcAKRPUjA0BgNVHREELTArggtzYW1sdGVz\ndC5pZIYcaHR0cHM6Ly9zYW1sdGVzdC5pZC9zYW1sL2lkcDANBgkqhkiG9w0BAQsF\nAAOCAQEABZDFRNtcbvIRmblnZItoWCFhVUlq81ceSQddLYs8DqK340//hWNAbYdj\nWcP85HhIZnrw6NGCO4bUipxZXhiqTA/A9d1BUll0vYB8qckYDEdPDduYCOYemKkD\ndmnHMQWs9Y6zWiYuNKEJ9mf3+1N8knN/PK0TYVjVjXAf2CnOETDbLtlj6Nqb8La3\nsQkYmU+aUdopbjd5JFFwbZRaj6KiHXHtnIRgu8sUXNPrgipUgZUOVhP0C0N5OfE4\nJW8ZBrKgQC/6vJ2rSa9TlzI6JAa5Ww7gMXMP9M+cJUNQklcq+SBnTK8G+uBHgPKR\nzBDsMIEzRtQZm4GIoHJae4zmnCekkQ==\n                        </ds:X509Certificate>\n                    </ds:X509Data>\n            </ds:KeyInfo>\n\n        </KeyDescriptor>\n\n        <ArtifactResolutionService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:SOAP\" Location=\"https://samltest.id/idp/profile/SAML2/SOAP/ArtifactResolution\" index=\"1\" />\n\n        <SingleLogoutService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect\" Location=\"https://samltest.id/idp/profile/SAML2/Redirect/SLO\"/>\n        <SingleLogoutService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" Location=\"https://samltest.id/idp/profile/SAML2/POST/SLO\"/>\n        <SingleLogoutService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST-SimpleSign\" Location=\"https://samltest.id/idp/profile/SAML2/POST-SimpleSign/SLO\"/>\n\n        <SingleSignOnService Binding=\"urn:mace:shibboleth:1.0:profiles:AuthnRequest\" Location=\"https://samltest.id/idp/profile/Shibboleth/SSO\"/>\n        <SingleSignOnService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" Location=\"https://samltest.id/idp/profile/SAML2/POST/SSO\"/>\n        <SingleSignOnService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST-SimpleSign\" Location=\"https://samltest.id/idp/profile/SAML2/POST-SimpleSign/SSO\"/>\n        <SingleSignOnService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect\" Location=\"https://samltest.id/idp/profile/SAML2/Redirect/SSO\"/>\n        <SingleSignOnService Binding=\"urn:oasis:names:tc:SAML:2.0:bindings:SOAP\" Location=\"https://samltest.id/idp/profile/SAML2/SOAP/ECP\"/>\n\n    </IDPSSODescriptor>\n\n</EntityDescriptor>\n",
+		EntityId:                   "http://example.test/service/rest/v1/security/saml/metadata",
+		ValidateAssertionSignature: false,
+		ValidateResponseSignature:  true,
+		UsernameAttribute:          "username2",
+		FirstNameAttribute:         "firstName",
+		LastNameAttribute:          "lastName",
+		EmailAttribute:             "email",
+		GroupsAttribute:            "groups",
+	}
+}
+
+func TestAccDataSourceSecuritySaml(t *testing.T) {
+	resName := "data.nexus_security_saml.acceptance"
+	saml := testAccDataSourceSecuritySaml()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceSecuritySAMLConfig(saml) + testAccDataSourceSecuritySamlConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resName, "idp_metadata", saml.IdpMetadata),
+					resource.TestCheckResourceAttr(resName, "entity_id", saml.EntityId),
+					resource.TestCheckResourceAttr(resName, "validate_response_signature", strconv.FormatBool(saml.ValidateResponseSignature)),
+					resource.TestCheckResourceAttr(resName, "validate_assertion_signature", strconv.FormatBool(saml.ValidateAssertionSignature)),
+					resource.TestCheckResourceAttr(resName, "username_attribute", saml.UsernameAttribute),
+					resource.TestCheckResourceAttr(resName, "first_name_attribute", saml.FirstNameAttribute),
+					resource.TestCheckResourceAttr(resName, "last_name_attribute", saml.LastNameAttribute),
+					resource.TestCheckResourceAttr(resName, "email_attribute", saml.EmailAttribute),
+					resource.TestCheckResourceAttr(resName, "groups_attribute", saml.GroupsAttribute),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceSecuritySamlConfig() string {
+	return fmt.Sprintf(`
+data "nexus_security_user" "acceptance" {
+}
+`)
+}
