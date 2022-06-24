a30618081075998e25833db4ead814576c4b340e
Andre Moeller <andre.moeller@datadrivers.de>
2022-06-24 09:01:36 +0200

    feat(repository): Add resource and data source for conda repository

---
+++ examples/resources/nexus_repository_conan_proxy/resource.tf
@@ -0,0 +1,25 @@
+resource "nexus_repository_conan_proxy" "conan_center" {
+  name   = "conan-center"
+  online = true
+
+  storage {
+    blob_store_name                = "default"
+    strict_content_type_validation = true
+  }
+
+  proxy {
+    remote_url       = "https://center.conan.io"
+    content_max_age  = 1440
+    metadata_max_age = 1440
+  }
+
+  negative_cache {
+    enabled      = true
+    time_to_live = 1440
+  }
+
+  http_client {
+    blocked    = false
+    auto_block = true
+  }
+}
---
+++ internal/services/repository/resource_repository_conda_proxy.go
@@ -0,0 +1,207 @@
+package repository
+
+import (
+	nexus "github.com/datadrivers/go-nexus-client/nexus3"
+	"github.com/datadrivers/go-nexus-client/nexus3/schema/repository"
+	"github.com/datadrivers/terraform-provider-nexus/internal/schema/common"
+	repositorySchema "github.com/datadrivers/terraform-provider-nexus/internal/schema/repository"
+	"github.com/datadrivers/terraform-provider-nexus/internal/tools"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func ResourceRepositoryCondaProxy() *schema.Resource {
+	return &schema.Resource{
+		Description: "Use this resource to create an conda proxy repository.",
+
+		Create: resourceCondaProxyRepositoryCreate,
+		Delete: resourceCondaProxyRepositoryDelete,
+		Exists: resourceCondaProxyRepositoryExists,
+		Read:   resourceCondaProxyRepositoryRead,
+		Update: resourceCondaProxyRepositoryUpdate,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			// Common schemas
+			"id":     common.ResourceID,
+			"name":   repositorySchema.ResourceName,
+			"online": repositorySchema.ResourceOnline,
+			// Proxy schemas
+			"cleanup":        repositorySchema.ResourceCleanup,
+			"http_client":    repositorySchema.ResourceHTTPClient,
+			"negative_cache": repositorySchema.ResourceNegativeCache,
+			"proxy":          repositorySchema.ResourceProxy,
+			"routing_rule":   repositorySchema.ResourceRoutingRule,
+			"storage":        repositorySchema.ResourceStorage,
+		},
+	}
+}
+
+func getCondaProxyRepositoryFromResourceData(resourceData *schema.ResourceData) repository.CondaProxyRepository {
+	httpClientConfig := resourceData.Get("http_client").([]interface{})[0].(map[string]interface{})
+	negativeCacheConfig := resourceData.Get("negative_cache").([]interface{})[0].(map[string]interface{})
+	proxyConfig := resourceData.Get("proxy").([]interface{})[0].(map[string]interface{})
+	storageConfig := resourceData.Get("storage").([]interface{})[0].(map[string]interface{})
+
+	repo := repository.CondaProxyRepository{
+		Name:   resourceData.Get("name").(string),
+		Online: resourceData.Get("online").(bool),
+		Storage: repository.Storage{
+			BlobStoreName:               storageConfig["blob_store_name"].(string),
+			StrictContentTypeValidation: storageConfig["strict_content_type_validation"].(bool),
+		},
+		HTTPClient: repository.HTTPClient{
+			AutoBlock: httpClientConfig["auto_block"].(bool),
+			Blocked:   httpClientConfig["blocked"].(bool),
+		},
+		NegativeCache: repository.NegativeCache{
+			Enabled: negativeCacheConfig["enabled"].(bool),
+			TTL:     negativeCacheConfig["ttl"].(int),
+		},
+		Proxy: repository.Proxy{
+			ContentMaxAge:  proxyConfig["content_max_age"].(int),
+			MetadataMaxAge: proxyConfig["metadata_max_age"].(int),
+			RemoteURL:      proxyConfig["remote_url"].(string),
+		},
+	}
+
+	if routingRule, ok := resourceData.GetOk("routing_rule"); ok {
+		repo.RoutingRule = tools.GetStringPointer(routingRule.(string))
+		repo.RoutingRuleName = tools.GetStringPointer(routingRule.(string))
+	}
+
+	cleanupList := resourceData.Get("cleanup").([]interface{})
+	if len(cleanupList) > 0 && cleanupList[0] != nil {
+		cleanupConfig := cleanupList[0].(map[string]interface{})
+		if len(cleanupConfig) > 0 {
+			policy_names, ok := cleanupConfig["policy_names"]
+			if ok {
+				repo.Cleanup = &repository.Cleanup{
+					PolicyNames: tools.InterfaceSliceToStringSlice(policy_names.(*schema.Set).List()),
+				}
+			}
+		}
+	}
+
+	if v, ok := httpClientConfig["authentication"]; ok {
+		authList := v.([]interface{})
+		if len(authList) == 1 && authList[0] != nil {
+			authConfig := authList[0].(map[string]interface{})
+
+			repo.HTTPClient.Authentication = &repository.HTTPClientAuthentication{
+				NTLMDomain: authConfig["ntlm_domain"].(string),
+				NTLMHost:   authConfig["ntlm_host"].(string),
+				Type:       repository.HTTPClientAuthenticationType(authConfig["type"].(string)),
+				Username:   authConfig["username"].(string),
+				Password:   authConfig["password"].(string),
+			}
+		}
+	}
+
+	if v, ok := httpClientConfig["connection"]; ok {
+		connectionList := v.([]interface{})
+		if len(connectionList) == 1 && connectionList[0] != nil {
+			connectionConfig := connectionList[0].(map[string]interface{})
+			repo.HTTPClient.Connection = &repository.HTTPClientConnection{
+				EnableCircularRedirects: tools.GetBoolPointer(connectionConfig["enable_circular_redirects"].(bool)),
+				EnableCookies:           tools.GetBoolPointer(connectionConfig["enable_cookies"].(bool)),
+				Retries:                 tools.GetIntPointer(connectionConfig["retries"].(int)),
+				Timeout:                 tools.GetIntPointer(connectionConfig["timeout"].(int)),
+				UserAgentSuffix:         connectionConfig["user_agent_suffix"].(string),
+				UseTrustStore:           tools.GetBoolPointer(connectionConfig["use_trust_store"].(bool)),
+			}
+		}
+	}
+
+	return repo
+}
+
+func setCondaProxyRepositoryToResourceData(repo *repository.CondaProxyRepository, resourceData *schema.ResourceData) error {
+	resourceData.SetId(repo.Name)
+	resourceData.Set("name", repo.Name)
+	resourceData.Set("online", repo.Online)
+
+	if repo.RoutingRuleName != nil {
+		resourceData.Set("routing_rule", repo.RoutingRuleName)
+	} else if repo.RoutingRule != nil {
+		resourceData.Set("routing_rule", repo.RoutingRule)
+	}
+
+	if err := resourceData.Set("storage", flattenStorage(&repo.Storage)); err != nil {
+		return err
+	}
+
+	if err := resourceData.Set("http_client", flattenHTTPClient(&repo.HTTPClient, resourceData)); err != nil {
+		return err
+	}
+
+	if err := resourceData.Set("negative_cache", flattenNegativeCache(&repo.NegativeCache)); err != nil {
+		return err
+	}
+
+	if err := resourceData.Set("proxy", flattenProxy(&repo.Proxy)); err != nil {
+		return err
+	}
+
+	if repo.Cleanup != nil {
+		if err := resourceData.Set("cleanup", flattenCleanup(repo.Cleanup)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func resourceCondaProxyRepositoryCreate(resourceData *schema.ResourceData, m interface{}) error {
+	client := m.(*nexus.NexusClient)
+
+	repo := getCondaProxyRepositoryFromResourceData(resourceData)
+
+	if err := client.Repository.Conda.Proxy.Create(repo); err != nil {
+		return err
+	}
+	resourceData.SetId(repo.Name)
+
+	return resourceCondaProxyRepositoryRead(resourceData, m)
+}
+
+func resourceCondaProxyRepositoryRead(resourceData *schema.ResourceData, m interface{}) error {
+	client := m.(*nexus.NexusClient)
+
+	repo, err := client.Repository.Conda.Proxy.Get(resourceData.Id())
+	if err != nil {
+		return err
+	}
+
+	if repo == nil {
+		resourceData.SetId("")
+		return nil
+	}
+
+	return setCondaProxyRepositoryToResourceData(repo, resourceData)
+}
+
+func resourceCondaProxyRepositoryUpdate(resourceData *schema.ResourceData, m interface{}) error {
+	client := m.(*nexus.NexusClient)
+
+	repoName := resourceData.Id()
+	repo := getCondaProxyRepositoryFromResourceData(resourceData)
+
+	if err := client.Repository.Conda.Proxy.Update(repoName, repo); err != nil {
+		return err
+	}
+
+	return resourceCondaProxyRepositoryRead(resourceData, m)
+}
+
+func resourceCondaProxyRepositoryDelete(resourceData *schema.ResourceData, m interface{}) error {
+	client := m.(*nexus.NexusClient)
+	return client.Repository.Conda.Proxy.Delete(resourceData.Id())
+}
+
+func resourceCondaProxyRepositoryExists(resourceData *schema.ResourceData, m interface{}) (bool, error) {
+	client := m.(*nexus.NexusClient)
+
+	repo, err := client.Repository.Conda.Proxy.Get(resourceData.Id())
+	return repo != nil, err
+}
