5cf88156393d5735d4cd0c75d45fd141f6ea8498
Nuno Lima <nuno.lima@zerog.aero>
2022-10-07 19:52:41 +0200

    Fix ttl variable name

--- examples/resources/nexus_repository_maven_proxy/resource.tf
+++ examples/resources/nexus_repository_maven_proxy/resource.tf
@@ -14,8 +14,8 @@ resource "nexus_repository_maven_proxy" "maven_central" {
   }
 
   negative_cache {
-    enabled      = true
-    time_to_live = 1440
+    enabled = true
+    ttl     = 1440
   }
 
   http_client {
