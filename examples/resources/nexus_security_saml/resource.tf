b8ac3f63659bfafd04e217c993c0053959cf0615
Nuno Lima <nuno.lima@zerog.aero>
2022-10-07 19:53:13 +0200

    Add missing enclosing characters

--- examples/resources/nexus_security_saml/resource.tf
+++ examples/resources/nexus_security_saml/resource.tf
@@ -1,11 +1,11 @@
 resource "nexus_security_saml" "example" {
-  idp_metadata                  = "<EntityDescriptor ...>...</EntityDescriptor>"
-  entity_id                     = "http://nexus.example/service/rest/v1/security/saml/metadata"
-  validate_response_signature   = true
-  validate_assertion_signature  = true
-  username_attribute            = "username"
-  first_name_attribute          = "firstName"
-  last_name_attribute           = "lastName
-  email_attribute               = "email
-  groups_attribute              = "groups"
+  idp_metadata                 = "<EntityDescriptor ...>...</EntityDescriptor>"
+  entity_id                    = "http://nexus.example/service/rest/v1/security/saml/metadata"
+  validate_response_signature  = true
+  validate_assertion_signature = true
+  username_attribute           = "username"
+  first_name_attribute         = "firstName"
+  last_name_attribute          = "lastName"
+  email_attribute              = "email"
+  groups_attribute             = "groups"
 }
