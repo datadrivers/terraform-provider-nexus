17847a745d1a229fa0c03ae559a0076303de4914
fabrue <fabian.bruegge@datadrivers.de>
2023-08-06 22:31:29 +0200

    feat: implement data source

--- internal/services/other/data_source_mail_config.go
+++ internal/services/other/data_source_mail_config.go
@@ -12,22 +12,66 @@ func DataSourceMailConfig() *schema.Resource {
 		Read: DataSourceMailConfigRead,
 		Schema: map[string]*schema.Schema{
 			"id": common.DataSourceID,
-			"enabled": {
-				Description: "Whether the mail config is active or not",
-				Computed:    true,
-				Type:        schema.TypeBool,
-			},
 			"host": {
-				Description: "Host",
-				Computed:    true,
+				Description: "hostname",
 				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"from_address": {
+				Description: "fromAddress",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"port": {
+				Description: "port",
+				Type:        schema.TypeInt,
+				Computed:    true,
+			},
+			"enabled": {
+				Description: "Whether the config is enabled or not",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"username": {
+				Description: "Username",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"subject_prefix": {
+				Description: "Subject prefix",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"start_tls_enabled": {
+				Description: "Star TLS Enabled",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"start_tls_required": {
+				Description: "Star TLS required",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"ssl_on_connect_enabled": {
+				Description: "SSL on connect enabled",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"ssl_server_identity_check_enabled": {
+				Description: "SSL on connect enabled",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"nexus_trust_store_enabled": {
+				Description: "SSL on connect enabled",
+				Type:        schema.TypeBool,
+				Computed:    true,
 			},
 		},
 	}
 }
 
 func DataSourceMailConfigRead(d *schema.ResourceData, m interface{}) error {
-	//d.SetId(d.Get("id").(string))
-	// d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+	d.SetId(MailConfigId)
 	return resourceMailConfigRead(d, m)
 }
