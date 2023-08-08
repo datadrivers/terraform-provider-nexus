9ec504162fa3ad88f2b03e634cc94330f173fdb9
fabrue <fabian.bruegge@datadrivers.de>
2023-08-08 09:34:32 +0200

    docs: add examples

---
+++ examples/resources/nexus_mail_config/resource.tf
@@ -0,0 +1,20 @@
+resource "nexus_mail_config" "config" {
+  port         = 25
+  host         = "examplehost.org"
+  from_address = "from@examplehost.org"
+
+  enabled                   = true
+  username                  = "uname"
+  password                  = "topsecret"
+  subject_prefix            = "prefix: "
+  start_tls_enabled         = true
+  start_tls_required        = true
+  ssl_on_connect_enabled    = true
+  nexus_trust_store_enabled = true
+}
+
+data "nexus_mail_config" "config" {}
+
+output "host" {
+  value = data.nexus_mail_config.config.host
+}
