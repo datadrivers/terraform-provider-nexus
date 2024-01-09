80cb42d815ca578156fbbeb655c848a67d228418
André Frimberger <andre@intellisoft.de>
2024-01-09 18:29:20 +0100

    feat: add resource and datasource for SSL truststore

---
+++ examples/data-sources/nexus_security_ssl_truststore/data-source.tf
@@ -0,0 +1,10 @@
+# Retrieve Cert via Nexus
+data "nexus_security_ssl_truststore" "ldap_cert" {
+  host = "google.de"
+  port = 443
+}
+
+# Import Cert into Nexus
+resource "nexus_security_ssl_truststore" "ldap_cert" {
+  pem = data.nexus_security_ssl_truststore.ldap_cert
+}
--- internal/tools/main.go
+++ internal/tools/main.go
@@ -1,10 +1,17 @@
 package tools
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"net/http"
 	"os"
 	"sort"
 	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -77,4 +84,55 @@ func FormatPrivilegeActionsForConfig[T any](actions []T) string {
 		stringActions = append(stringActions, fmt.Sprintf("\"%v\"", action))
 	}
 	return strings.Join(stringActions, ", ")
+}
+
+func TestGetCertificateFingerprint(cert *x509.Certificate) (string, error) {
+	fingerprintBytes := sha1.Sum(cert.Raw)
+	var fingerprint bytes.Buffer
+	for i, f := range fingerprintBytes {
+		if i > 0 {
+			fmt.Fprintf(&fingerprint, ":")
+		}
+		fmt.Fprintf(&fingerprint, "%02X", f)
+	}
+	return fingerprint.String(), nil
+}
+
+func TestGenerateRandomCertificate() (string, *x509.Certificate, string, string, error) {
+	certPEM, privateCertPEM, _ := acctest.RandTLSCert("acctest")
+	certBlock, _ := pem.Decode([]byte(certPEM))
+	cert, _ := x509.ParseCertificate(certBlock.Bytes)
+	certFingerprint, _ := TestGetCertificateFingerprint(cert)
+	return certPEM, cert, certFingerprint, privateCertPEM, nil
+}
+
+func TestRetrieveCert(url string) (*x509.Certificate, error) {
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, clientErr := client.Get(url)
+	if clientErr != nil {
+		return nil, clientErr
+	}
+	if resp.TLS != nil {
+		certificates := resp.TLS.PeerCertificates
+		if len(certificates) > 0 {
+			return certificates[0], nil
+		}
+	}
+
+	return nil, fmt.Errorf("failed retrieving certificate from '%s'", url)
+}
+
+func TestPemEncode(b []byte, block string) (string, error) {
+	var buf bytes.Buffer
+	pb := &pem.Block{Type: block, Bytes: b}
+	if err := pem.Encode(&buf, pb); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
