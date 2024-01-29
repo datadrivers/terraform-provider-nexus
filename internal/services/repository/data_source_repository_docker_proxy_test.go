621abc1a49e352a044c8a5f5274f6b3d10fff1ed
Robert Tribit <Robert.Tribit@citizensbank.com>
2024-01-29 15:03:14 -0600

    Fixing pro tests for docker proxy.

--- internal/services/repository/data_source_repository_docker_proxy_test.go
+++ internal/services/repository/data_source_repository_docker_proxy_test.go
@@ -88,6 +88,7 @@ func TestAccProDataSourceRepositoryDockerProxy(t *testing.T) {
 		Docker: repository.Docker{
 			ForceBasicAuth: true,
 			V1Enabled:      true,
+			Subdomain:      tools.GetStringPointer(name),
 		},
 		DockerProxy: repository.DockerProxy{
 			IndexType: repository.DockerProxyIndexTypeHub,
@@ -119,6 +120,7 @@ func TestAccProDataSourceRepositoryDockerProxy(t *testing.T) {
 						resource.TestCheckResourceAttr(dataSourceName, "docker.#", "1"),
 						resource.TestCheckResourceAttr(dataSourceName, "docker.0.force_basic_auth", strconv.FormatBool(repoUsingDefaults.Docker.ForceBasicAuth)),
 						resource.TestCheckResourceAttr(dataSourceName, "docker.0.v1_enabled", strconv.FormatBool(repoUsingDefaults.Docker.V1Enabled)),
+						resource.TestCheckResourceAttr(dataSourceName, "docker.0.subdomain", string(*repo.Docker.Subdomain)),
 						resource.TestCheckResourceAttr(dataSourceName, "docker_proxy.#", "1"),
 						resource.TestCheckResourceAttr(dataSourceName, "docker_proxy.0.index_type", string(repoUsingDefaults.DockerProxy.IndexType)),
 					),
