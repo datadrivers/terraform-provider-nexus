c6c6f69ad9c3dc76fd1655f8cbf9837b8d7eac44
Robert Tribit <Robert.Tribit@citizensbank.com>
2024-01-29 14:20:34 -0600

    Adding logic to handle skipping pro tests.

--- internal/services/repository/data_source_repository_docker_group_test.go
+++ internal/services/repository/data_source_repository_docker_group_test.go
@@ -31,18 +31,12 @@ func TestAccDataSourceRepositoryDockerGroup(t *testing.T) {
 		Docker: repository.Docker{
 			ForceBasicAuth: true,
 			V1Enabled:      true,
-			Subdomain:      repoHosted.Subdomain,
 		},
 		Group: repository.GroupDeploy{
 			MemberNames: []string{repoHosted.Name},
 		},
 	}
 	dataSourceName := "data.nexus_repository_docker_group.acceptance"
-
-	subdomain := ""
-	if tools.GetEnv("SKIP_PRO_TESTS", "false") == "false" {
-		subdomain = string(*repoGroup.Docker.Subdomain)
-	}
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { acceptance.AccPreCheck(t) },
@@ -61,7 +55,63 @@ func TestAccDataSourceRepositoryDockerGroup(t *testing.T) {
 							resource.TestCheckResourceAttr(dataSourceName, "docker.#", "1"),
 							resource.TestCheckResourceAttr(dataSourceName, "docker.0.force_basic_auth", strconv.FormatBool(repoGroup.Docker.ForceBasicAuth)),
 							resource.TestCheckResourceAttr(dataSourceName, "docker.0.v1_enabled", strconv.FormatBool(repoGroup.Docker.V1Enabled)),
-							resource.TestCheckResourceAttr(dataSourceName, "docker.0.subdomain", subdomain),
+						),
+						resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "storage.#", "1"),
+							resource.TestCheckResourceAttr(dataSourceName, "storage.0.blob_store_name", repoGroup.Storage.BlobStoreName),
+							resource.TestCheckResourceAttr(dataSourceName, "storage.0.strict_content_type_validation", strconv.FormatBool(repoGroup.Storage.StrictContentTypeValidation)),
+							resource.TestCheckResourceAttr(dataSourceName, "group.#", "1"),
+							resource.TestCheckResourceAttr(dataSourceName, "group.0.member_names.#", "1"),
+							resource.TestCheckResourceAttr(dataSourceName, "group.0.member_names.0", repoGroup.Group.MemberNames[0]),
+						),
+					),
+				),
+			},
+		},
+	})
+}
+
+func TestAccProDataSourceRepositoryDockerGroup(t *testing.T) {
+	if tools.GetEnv("SKIP_PRO_TESTS", "false") == "true" {
+		t.Skip("Skipping Nexus Pro Tests")
+	}
+	repoHosted := testAccProResourceRepositoryDockerHosted()
+	repoGroup := repository.DockerGroupRepository{
+		Name:   fmt.Sprintf("acceptance-%s", acctest.RandString(10)),
+		Online: true,
+		Storage: repository.Storage{
+			BlobStoreName:               "default",
+			StrictContentTypeValidation: false,
+		},
+		Docker: repository.Docker{
+			ForceBasicAuth: true,
+			V1Enabled:      true,
+			Subdomain:      repoHosted.Subdomain,
+		},
+		Group: repository.GroupDeploy{
+			MemberNames: []string{repoHosted.Name},
+		},
+	}
+	dataSourceName := "data.nexus_repository_docker_group.acceptance"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acceptance.AccPreCheck(t) },
+		Providers: acceptance.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceRepositoryDockerHostedConfig(repoHosted) + testAccResourceRepositoryDockerGroupConfig(repoGroup) + testAccDataSourceRepositoryDockerGroupConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.ComposeAggregateTestCheckFunc(
+						resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "id", repoGroup.Name),
+							resource.TestCheckResourceAttr(dataSourceName, "name", repoGroup.Name),
+							resource.TestCheckResourceAttr(dataSourceName, "online", strconv.FormatBool(repoGroup.Online)),
+						),
+						resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "docker.#", "1"),
+							resource.TestCheckResourceAttr(dataSourceName, "docker.0.force_basic_auth", strconv.FormatBool(repoGroup.Docker.ForceBasicAuth)),
+							resource.TestCheckResourceAttr(dataSourceName, "docker.0.v1_enabled", strconv.FormatBool(repoGroup.Docker.V1Enabled)),
+							resource.TestCheckResourceAttr(dataSourceName, "docker.0.subdomain", *repoGroup.Docker.Subdomain),
 						),
 						resource.ComposeAggregateTestCheckFunc(
 							resource.TestCheckResourceAttr(dataSourceName, "storage.#", "1"),
--- internal/services/repository/resource_repository_docker_hosted_test.go
+++ internal/services/repository/resource_repository_docker_hosted_test.go
@@ -18,6 +18,32 @@ import (
 func testAccResourceRepositoryDockerHosted() repository.DockerHostedRepository {
 	writePolicy := repository.StorageWritePolicyAllow
 
+	return repository.DockerHostedRepository{
+		Name:   fmt.Sprintf("test-repo-%s", acctest.RandString(10)),
+		Online: true,
+		Docker: repository.Docker{
+			ForceBasicAuth: true,
+			HTTPPort:       tools.GetIntPointer(rand.Intn(999) + 32000),
+			HTTPSPort:      tools.GetIntPointer(rand.Intn(999) + 33000),
+			V1Enabled:      false,
+		},
+		Storage: repository.HostedStorage{
+			BlobStoreName:               "default",
+			StrictContentTypeValidation: true,
+			WritePolicy:                 &writePolicy,
+		},
+		Cleanup: &repository.Cleanup{
+			PolicyNames: []string{"cleanup-weekly"},
+		},
+		Component: &repository.Component{
+			ProprietaryComponents: true,
+		},
+	}
+}
+
+func testAccProResourceRepositoryDockerHosted() repository.DockerHostedRepository {
+	writePolicy := repository.StorageWritePolicyAllow
+
 	subdomain := fmt.Sprintf("test-repo-%s", acctest.RandString(10))
 
 	return repository.DockerHostedRepository{
@@ -54,7 +80,58 @@ func testAccResourceRepositoryDockerHostedConfig(repo repository.DockerHostedRep
 }
 
 func TestAccResourceRepositoryDockerHosted(t *testing.T) {
+	if tools.GetEnv("SKIP_PRO_TESTS", "false") == "true" {
+		t.Skip("Skipping Nexus Pro tests")
+	}
 	repo := testAccResourceRepositoryDockerHosted()
+	resourceName := "nexus_repository_docker_hosted.acceptance"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acceptance.AccPreCheck(t) },
+		Providers: acceptance.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceRepositoryDockerHostedConfig(repo),
+				Check: resource.ComposeTestCheckFunc(
+					resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(resourceName, "id", repo.Name),
+						resource.TestCheckResourceAttr(resourceName, "name", repo.Name),
+						resource.TestCheckResourceAttr(resourceName, "online", strconv.FormatBool(repo.Online)),
+					),
+					resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(resourceName, "storage.#", "1"),
+						resource.TestCheckResourceAttr(resourceName, "storage.0.blob_store_name", repo.Storage.BlobStoreName),
+						resource.TestCheckResourceAttr(resourceName, "storage.0.strict_content_type_validation", strconv.FormatBool(repo.Storage.StrictContentTypeValidation)),
+						resource.TestCheckResourceAttr(resourceName, "storage.0.write_policy", string(*repo.Storage.WritePolicy)),
+						resource.TestCheckResourceAttr(resourceName, "cleanup.#", "1"),
+						resource.TestCheckResourceAttr(resourceName, "cleanup.0.policy_names.#", "1"),
+						resource.TestCheckResourceAttr(resourceName, "cleanup.0.policy_names.0", repo.Cleanup.PolicyNames[0]),
+						resource.TestCheckResourceAttr(resourceName, "component.#", "1"),
+						resource.TestCheckResourceAttr(resourceName, "component.0.proprietary_components", strconv.FormatBool(repo.Component.ProprietaryComponents)),
+					),
+					resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr(resourceName, "docker.#", "1"),
+						resource.TestCheckResourceAttr(resourceName, "docker.0.force_basic_auth", strconv.FormatBool(repo.Docker.ForceBasicAuth)),
+						resource.TestCheckResourceAttr(resourceName, "docker.0.http_port", strconv.Itoa(*repo.Docker.HTTPPort)),
+						resource.TestCheckResourceAttr(resourceName, "docker.0.https_port", strconv.Itoa(*repo.Docker.HTTPSPort)),
+						resource.TestCheckResourceAttr(resourceName, "docker.0.v1_enabled", strconv.FormatBool(repo.Docker.V1Enabled)),
+					),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportStateId:     repo.Name,
+				ImportState:       true,
+				ImportStateVerify: true,
+				// Signing block is not returned
+				ImportStateVerifyIgnore: []string{},
+			},
+		},
+	})
+}
+
+func TestAccProResourceRepositoryDockerHosted(t *testing.T) {
+	repo := testAccProResourceRepositoryDockerHosted()
 	resourceName := "nexus_repository_docker_hosted.acceptance"
 
 	resource.Test(t, resource.TestCase{
