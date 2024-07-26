2a64968b1a25cbd541c6de61f7f65d7ab95f9932
Alex Shearn <244545+shearn89@users.noreply.github.com>
2024-07-26 13:56:02 +0100

    Remove publihser to external repo (#3)
    * Update various notes and example files
    
    * Add the first draft of the publisher tool
    
    * Pass in args
    
    * Fix a bunch of bugs from testing
    
    * Add publisher to makefile
    
    * Removing publisher, moving to other repo
    
    * fixup! Removing publisher, moving to other repo

--- deployment/get-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Omit the leading 'v' if present:
-version="2.4.0-rc2-bump"
-
-wget "https://github.com/octoenergy/terraform-provider-nexus/releases/download/v${version}/terraform-provider-nexus_${version}_SHA256SUMS"
-wget "https://github.com/octoenergy/terraform-provider-nexus/releases/download/v${version}/terraform-provider-nexus_${version}_SHA256SUMS.sig"
-wget "https://github.com/octoenergy/terraform-provider-nexus/releases/download/v${version}/terraform-provider-nexus_${version}_linux_amd64.zip"
